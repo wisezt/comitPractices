0459e30625bf69f4e1d014f7e68678d464e5560f
Ting <wisezt@hotmail.com>
2018-07-11 11:41:18 -0500

    try

---
+++ july6/Bicycle.java
@@ -0,0 +1,36 @@
+package july6;
+
+public class Bicycle {
+	
+	private int speed;
+	
+	private String color;
+	
+	private String gear;
+	
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getGear() {
+		return gear;
+	}
+
+	public void setGear(String gear) {
+		this.gear = gear;
+	}
+
+	public void setSpeed(int speed){
+		this.speed = speed;
+	}
+	
+	public int getSpeed(){
+		return speed;
+	}
+	
+	
+}
